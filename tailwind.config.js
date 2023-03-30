df220f6e635e4f27bdd56ac84788a16ae4b03fb3
Mina <alsdk9879@infinitum.co.kr>
2023-03-30 18:09:46 +0900

    tailwind main02 반응형 작업 중

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,38 +5,39 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      'pretendard': ['Pretendard']
+      'pretendard': ['Pretendard'],
+      'hri_iconset': ['hri_iconset']
+    },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 992px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1200px) { ... }
+
+      '2xl': '1400px',
+      // => @media (min-width: 1400px) { ... }
     },
     // screens: {
-    //   'sm': '576px',
-    //   // => @media (min-width: 576px) { ... }
-
-    //   'md': '768px',
-    //   // => @media (min-width: 768px) { ... }
-
-    //   'lg': '992px',
-    //   // => @media (min-width: 992px) { ... }
-
-    //   'xl': '1200px',
-    //   // => @media (min-width: 1200px) { ... }
-
-    //   '2xl': '1400px',
-    //   // => @media (min-width: 1400px) { ... }
-    // },
-    // screens: {
-    //   '2xl': '1400px',
+    //   '2xl': {'max': '1400px'},
     //   // => @media (max-width: 1400px) { ... }
 
-    //   'xl': '1200px',
+    //   'xl': {'max': '1200px'},
     //   // => @media (max-width: 1200px) { ... }
 
-    //   'lg': '992px',
+    //   'lg': {'max': '992px'},
     //   // => @media (max-width: 992px) { ... }
 
-    //   'md': '768px',
+    //   'md': {'max': '768px'},
     //   // => @media (max-width: 768px) { ... }
 
-    //   'sm': '576px',
+    //   'sm': {'max': '576px'},
     //   // => @media (max-width: 576px) { ... }
     // }
   },
