17e694af8fa7abcb438ecc4455a454dea3c15dfe
Mina <alsdk9879@infinitum.co.kr>
2023-04-13 16:33:04 +0900

    portfolio scss 컴파일 분리

--- assets/js/portfolio01.js
+++ assets/js/portfolio01.js
@@ -1,11 +1,11 @@
 // 마우스커서 움직임 효과
-const cursor = document.querySelector(".cursor");
-const follower = document.querySelector(".cursor-follower");
+// const cursor = document.querySelector(".cursor");
+// const follower = document.querySelector(".cursor-follower");
 
-document.addEventListener("mousemove", (e) => {
-    gsap.to(cursor, {duration: 0.3, left: e.pageX -5, top:e.pageY -5})
-    gsap.to(follower, {duration: 0.8, left: e.pageX -15, top:e.pageY -15})
-});
+// document.addEventListener("mousemove", (e) => {
+//     gsap.to(cursor, {duration: 0.3, left: e.pageX -5, top:e.pageY -5})
+//     gsap.to(follower, {duration: 0.8, left: e.pageX -15, top:e.pageY -15})
+// });
 
 
 // header 상단바 스크롤시 hide & show
