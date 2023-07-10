e3194adc6e92e4d9dc9e3769252ce99f8eeb175f
Mina <alsdk9879@infinitum.co.kr>
2023-07-10 17:47:19 +0900

    weather

--- assets/js/clonecoding/weather.js
+++ assets/js/clonecoding/weather.js
@@ -1,9 +1,16 @@
-const btnWeather = document.querySelector('.btn-weather');
+// const btnWeather = document.querySelector('.btn-weather');
 
-btnWeather.addEventListener('click', () => {
-  navigator.geolocation.getCurrentPosition(success);
-});
+// btnWeather.addEventListener('click', () => {
+//   navigator.geolocation.getCurrentPosition(success, fail);
+// });
 
-const success = (position) => {
-  console.log(position)
-}
+// const success = (position) => {
+//   console.log(position)
+// }
+
+// const fail = () => {
+//   alert('no')
+// }
+
+// 위도: 37.5665° N
+// 경도: 126.9780° E
