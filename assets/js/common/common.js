79e858a3af8dab2a3e43cc35c6a1951967f7b466
Mina <alsdk9879@infinitum.co.kr>
2024-03-14 16:31:33 +0900

    toyproject - toDoList 진행중

---
+++ assets/js/common/common.js
@@ -0,0 +1,19 @@
+/*
+ * tab menu
+ */
+const tabList = document.querySelectorAll('.tabmenu-list button');
+const tabCont = document.querySelectorAll('.tabmenu-cont');
+
+tabList.forEach((tab, index) => {
+  tab.addEventListener('click', function () {
+    tabList.forEach((el) => {
+      el.classList.remove('active');
+    });
+    tabList[index].classList.add('active');
+
+    tabCont.forEach((cont) => {
+      cont.classList.remove('active');
+    });
+    // tabCont[index].classList.add('active');
+  });
+});
--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,13 +1,16 @@
+/*
+  side-menubar open & fold
+*/
 document.querySelector('#header .btn-menubar').addEventListener('click', () => {
   document.querySelector('#nav').classList.add('open');
 });
 
 document.querySelector('#nav .btn-menubar').addEventListener('click', () => {
-  if( window.innerWidth > 1299.98 ) {
+  if (window.innerWidth > 1299.98) {
     document.querySelector('#layout').classList.toggle('fold');
   } else {
     document.querySelector('#nav').classList.toggle('open');
-  };
+  }
 });
 
 document.querySelector('#nav .btn-close').addEventListener('click', () => {
@@ -15,7 +18,7 @@ document.querySelector('#nav .btn-close').addEventListener('click', () => {
 });
 
 window.addEventListener('resize', () => {
-  if( window.innerWidth <= 1299.98 ) {
+  if (window.innerWidth <= 1299.98) {
     document.querySelector('#layout').classList.remove('fold');
-  };
+  }
 });
