a85df188e78bb577d6e29abead8e85ab75e6e5e3
Mina <alsdk9879@infinitum.co.kr>
2024-06-18 16:28:09 +0900

    portfolio > works > 프로젝트 팝업 작업중

--- assets/js/portfolio/portfolio01.js
+++ assets/js/portfolio/portfolio01.js
@@ -49,18 +49,6 @@ function onToggleAccordion(e) {
 accordionHeader && accordionHeader.forEach((el) => el.addEventListener('click', onToggleAccordion));
 
 /*
- work > 각 프로젝트 클릭시 modal open & close
-*/
-
-const project = document.querySelectorAll('.works .work-list .item');
-
-project.forEach((el, index) => {
-  el.addEventListener('click', () => {
-    console.log('click');
-  });
-});
-
-/*
  about > 스크롤시 해당 섹션에 따라 우측 snb active
 */
 
@@ -96,6 +84,89 @@ window.onscroll = function () {
     document.querySelectorAll('.snb .snb-list').forEach((el) => {
       el.classList.remove('active');
     });
-    lastSnbList.classList.add('active');
+    lastSnbList && lastSnbList.classList.add('active');
   }
 };
+
+/*
+ work > 각 프로젝트 클릭시 modal open & close
+*/
+
+// 모달 내  프로젝트 상세정보
+const popList = [
+  {
+    id: 'pop1',
+    title: '프로젝트명',
+    subTitle: '반응형 / 2024.06',
+    desc: '프로젝트 설명',
+    btnViewPC: 'PC화면 보러가기',
+    btnViewMO: '모바일화면 보러가기',
+    viewPCLink: '#',
+    viewMOLink: '#',
+    imgCont: './',
+    hideText: '닫기',
+  },
+  {
+    id: 'pop2',
+    title: '프로젝트명',
+    subTitle: '반응형 / 2024.06',
+    desc: '프로젝트 설명',
+    btnViewPC: 'PC화면 보러가기',
+    btnViewMO: '모바일화면 보러가기',
+    viewPCLink: '#',
+    viewMOLink: '#',
+    imgCont: './',
+    hideText: '닫기',
+  },
+];
+
+popList.forEach((el, index) => {
+  const popCont = document.querySelector('.pop-wrap').insertAdjacentHTML(
+    'beforeend',
+    `<div class="pop" id="${popList[index].id}">
+      <div class="pop-cont">
+        <div class="txt-wrap">
+          <h2 class="title">${popList[index].title}</h2>
+          <p class="sub-title">${popList[index].subTitle}</p>
+          <p class="desc">${popList[index].desc}</p>
+          <div class="btn-wrap">
+            <a href="${popList[index].viewPCLink}" class="btn-view-pc">PC화면 보러가기</a>
+            <a href="${popList[index].viewMOLink}" target="_blank" class="btn-view-mo">모바일화면 보러가기</a>
+          </div>
+        </div>
+        <div class="img-wrap">
+          <img src="${popList[index].imgCont}" alt="img-cont">
+        </div>
+        <button type="button" class="btn-close"><span class="hide">닫기</span></button>
+      </div>
+    </div>`
+  );
+});
+
+const itemProject = document.querySelectorAll('.works .work-list .item');
+const pop = document.querySelectorAll('.pop');
+const btnClose = document.querySelectorAll('.pop .btn-close');
+const bodyWorks = document.querySelector('#portfolio01.works');
+
+console.log(bodyWorks);
+
+// popup open
+itemProject.forEach((el, index) => {
+  el.addEventListener('click', () => {
+    pop.forEach((el) => {
+      el.classList.remove('open');
+    });
+    pop[index].classList.add('open');
+    bodyWorks.classList.add('pop-open');
+  });
+});
+
+// popup close
+btnClose.forEach((el, index) => {
+  el.addEventListener('click', () => {
+    pop.forEach((el) => {
+      el.classList.remove('open');
+    });
+    bodyWorks.classList.remove('pop-open');
+  });
+});
