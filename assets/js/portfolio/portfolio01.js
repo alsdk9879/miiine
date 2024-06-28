dc3c24f3405fd81107e7630635e04ef6d50779e4
Mina <alsdk9879@infinitum.co.kr>
2024-06-28 16:29:52 +0900

    portfolio > 모달 내  프젝트 상세정보 작성중

--- assets/js/portfolio/portfolio01.js
+++ assets/js/portfolio/portfolio01.js
@@ -101,8 +101,8 @@ const popList = [
     desc: '통일부와 지역사회가 함께하는 통일 참여 소통 종합 플렛폼인 통일플러스센터 웹 사이트를 유지보수하고 있습니다. <br> 기여도 : 100%',
     btnViewPC: 'PC화면 보러가기',
     btnViewMO: '모바일화면 보러가기',
-    viewPCLink: '#',
-    viewMOLink: '#',
+    viewPCLink: 'https://www.unipluscenter.go.kr/web/main',
+    viewMOLink: 'https://www.unipluscenter.go.kr/web/main',
     imgCont: './',
     hideText: '닫기',
   },
@@ -113,8 +113,8 @@ const popList = [
     desc: '프로젝트 설명',
     btnViewPC: 'PC화면 보러가기',
     btnViewMO: '모바일화면 보러가기',
-    viewPCLink: '#',
-    viewMOLink: '#',
+    viewPCLink: 'https://www.myhome.go.kr/hws/portal/main/getMgtMainHubPage.do',
+    viewMOLink: 'https://m.myhome.go.kr/hws/portal/main/getMgtMainHubPage.do',
     imgCont: './',
     hideText: '닫기',
   },
@@ -161,8 +161,8 @@ const popList = [
     desc: '프로젝트 설명',
     btnViewPC: 'PC화면 보러가기',
     btnViewMO: '모바일화면 보러가기',
-    viewPCLink: '#',
-    viewMOLink: '#',
+    viewPCLink: 'https://infinitum.co.kr/',
+    viewMOLink: 'https://infinitum.co.kr/',
     imgCont: './',
     hideText: '닫기',
   },
@@ -197,8 +197,8 @@ const popList = [
     desc: '프로젝트 설명',
     btnViewPC: 'PC화면 보러가기',
     btnViewMO: '모바일화면 보러가기',
-    viewPCLink: '#',
-    viewMOLink: '#',
+    viewPCLink: 'https://www.heypoll.co.kr/',
+    viewMOLink: 'https://www.heypoll.co.kr/mobile',
     imgCont: './',
     hideText: '닫기',
   },
@@ -209,8 +209,8 @@ const popList = [
     desc: '프로젝트 설명',
     btnViewPC: 'PC화면 보러가기',
     btnViewMO: '모바일화면 보러가기',
-    viewPCLink: '#',
-    viewMOLink: '#',
+    viewPCLink: 'https://www.homecc.com/',
+    viewMOLink: 'https://www.homecc.com/',
     imgCont: './',
     hideText: '닫기',
   },
@@ -219,21 +219,21 @@ const popList = [
 popList.forEach((el, index) => {
   const popCont = document.querySelector('.pop-wrap').insertAdjacentHTML(
     'beforeend',
-    `<div class="pop" id="${popList[index].id}">
-      <div class="pop-cont">
-        <div class="txt-wrap">
-          <h2 class="title">${popList[index].title}</h2>
-          <p class="sub-title">${popList[index].subTitle}</p>
-          <p class="desc">${popList[index].desc}</p>
-          <div class="btn-wrap">
-            <a href="${popList[index].viewPCLink}" target="_blank" class="btn-view-pc">PC화면 보러가기</a>
-            <a href="${popList[index].viewMOLink}" target="_blank" class="btn-view-mo">모바일화면 보러가기</a>
+    `<div class='pop' id='${popList[index].id}'>
+      <div class='pop-cont'>
+        <div class='txt-wrap'>
+          <h2 class='title'>${popList[index].title}</h2>
+          <p class='sub-title'>${popList[index].subTitle}</p>
+          <p class='desc'>${popList[index].desc}</p>
+          <div class='btn-wrap'>
+            <a href='${popList[index].viewPCLink}' target='_blank' class='btn-view-pc'>PC화면 보러가기</a>
+            <a href='${popList[index].viewMOLink}' target='_blank' class='btn-view-mo'>모바일화면 보러가기</a>
           </div>
         </div>
-        <div class="img-wrap">
-          <img src="${popList[index].imgCont}" alt="img-cont">
+        <div class='img-wrap'>
+          <img src='${popList[index].imgCont}' alt='img-cont'>
         </div>
-        <button type="button" class="btn-close"><span class="hide">닫기</span></button>
+        <button type='button' class='btn-close'><span class='hide'>닫기</span></button>
       </div>
     </div>`
   );
@@ -244,8 +244,6 @@ const pop = document.querySelectorAll('.pop');
 const btnClose = document.querySelectorAll('.pop .btn-close');
 const bodyWorks = document.querySelector('#portfolio01.works');
 
-console.log(bodyWorks);
-
 // popup open
 itemProject.forEach((el, index) => {
   el.addEventListener('click', () => {
@@ -254,6 +252,11 @@ itemProject.forEach((el, index) => {
     });
     pop[index].classList.add('open');
     bodyWorks.classList.add('pop-open');
+
+    if (window.innerWidth < 1200) {
+      pop[index].classList.remove('open');
+      bodyWorks.classList.remove('pop-open');
+    }
   });
 });
 
